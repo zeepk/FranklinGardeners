9f16022af8d0385fd3cdeb5c90f176ca14605dbd
Matt Hughes <mhughes.ipod@gmail.com>
2023-02-26 15:34:27 -0800

    logo

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,16 @@ module.exports = {
       dropShadow: {
         dark: "0px 5px 10px black",
       },
+      colors: {
+        bg: {
+          100: "#e595a4",
+          300: "#9CBFA7",
+          500: "#536E58",
+        },
+        text: {
+          500: "#292929",
+        },
+      },
     },
   },
   plugins: [],
