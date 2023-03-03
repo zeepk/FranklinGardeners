dc1c9e64f1e1bfbc4091c85dc17d63f61f6d56dc
Matt Hughes <mhughes.ipod@gmail.com>
2023-03-02 21:32:30 -0800

    hover

--- src/components/BoothSquare.tsx
+++ src/components/BoothSquare.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import type { Booth } from "../utils";
 
 type Props = Booth;
@@ -10,6 +11,7 @@ export default function BoothSquare({
   height = 4,
   rotation = 0,
 }: Props) {
+  const [showTooltip, setShowTooltip] = useState(false);
   const style = {
     top: `${yOffset}%`,
     left: `${xOffset}%`,
@@ -21,7 +23,15 @@ export default function BoothSquare({
     <div
       style={style}
       className="flex align-center justify-center text-center font-semibold absolute bg-cyan-500 border-2 border-cyan-300"
+      onClick={() => setShowTooltip(true)}
+      onMouseOver={() => setShowTooltip(true)}
+      onMouseLeave={() => setShowTooltip(false)}
     >
+      {showTooltip && (
+        <div className="absolute w-40 top-[-38px] bg-green-500 border-2 border-green-300">
+          {"Sample text tooltip"}
+        </div>
+      )}
       {id}
     </div>
   );
